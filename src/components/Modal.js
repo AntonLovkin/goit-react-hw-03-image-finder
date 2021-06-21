4d21dcf685dcfccf9bf4c11acbcdebbb6058fd9d
AntonLovkin <anton28111987@gmail.com>
2021-06-21 16:25:34 +0300

    correction modal

---
+++ src/components/Modal.js
@@ -0,0 +1,36 @@
+import { Component } from "react";
+
+export default class Modal extends Component {
+    componentDidMount() {
+        console.log("modal didmount");
+        window.addEventListener('keydown', this.handleKeyDown 
+        )
+    }
+
+    componentWillUnmount() {
+        console.log("modal willunmount");
+        window.removeEventListener('keydown', this.handleKeyDown)
+    }
+
+    handleKeyDown = evt => {
+        if (evt.code === 'Escape') {
+            this.props.onClose()
+        }
+    }
+
+    handleBackdropClick = evt => {
+        if (evt.currentTarget === evt.target) {
+            this.props.onClose()
+        }
+    }
+
+    render() {
+        return (
+        <div className="Overlay " onClick={this.handleBackdropClick}>
+                <div className="Modal">
+                    {this.props.children}
+            </div>
+        </div>
+        )
+    }
+}
